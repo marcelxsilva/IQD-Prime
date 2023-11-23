a898f790297c4e01df14086501acb2fe65a8e2b4
marcelxsilva <maarcelomonteiro@gmail.com>
2023-11-23 12:03:54 -0300

    home

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Button } from '@material-ui/core';
-import { AppInfo, Header } from '~/components';
+import { AppInfo, Chat, Header } from '~/components';
 import { Content } from './styles';
 
 type Props = {
@@ -10,6 +10,7 @@ type Props = {
 const Home: FC<Props> = ({ onLogout }): JSX.Element => (
   <Content>
     <Header />
+    <Chat />
   </Content>
 );
 
--- src/scenes/Home/styles.ts
+++ src/scenes/Home/styles.ts
@@ -6,4 +6,6 @@ export const Content = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: ${primaryDark};
+  display: flex;
+  flex-direction: column;
 `;
